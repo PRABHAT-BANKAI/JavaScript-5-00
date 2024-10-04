37f2f6b7bcd83c4c36091576794c1857d226ca33
PRABHAT-BANKAI <prabhssgg3@gmail.com>
2024-10-04 18:45:59 +0530

    revision

--- day5logic/index.js
+++ day5logic/index.js
@@ -18,3 +18,62 @@ let num2 = 12;
 let num3 = "1234566";
 
 console.log(num3 + num + num2);
+
+console.log(15 - 5 + 10 - 2 * 2 - (7 / 2) * (4 - 2));
+console.log(7 / 2);
+
+console.log(12 + "12");
+
+let numValue = "12345"; // immutable
+
+let str = "asd";
+let store = str + numValue;
+let arr = [1, 2, 3, 4]; // mutable
+
+// immutable mutable
+//implicit explicit
+
+//15-5+10-2*2-7/2*2
+//15-5+10-2*2-3*2
+
+//15-15-4-6
+
+// console.log(3*4*2*2)
+
+console.log(45 > 56 + 12 < 4 + 1 - 5 > 1 + 2); //false>true>3 // false>3 //false
+
+let x = 15;
+let y = 5;
+
+let tempValue = 0;
+tempValue = x;
+
+// x = y;
+// y = tempValue;
+
+x = x + y; //20
+
+y = x - y; //20-5 // 15
+
+x = x - y; //20-15 //5
+console.log(x, y);
+
+let english = 10;
+let math = 15;
+let javscript = 25;
+
+let average = english + math + javscript / 3;
+console.log(average);
+
+//0.5 //1//1.2//1
+console.log(Math.round(5.5));
+console.log(Math.ceil(5.3));
+console.log(Math.floor(5.5));
+
+let num5 = 56
+let num4 = 59
+
+// Math.min
+// Math.max
+// Math.random
+// Math.sqrt
