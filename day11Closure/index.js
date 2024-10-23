fb4c3681f9e8645ee9024842bc8a1fd0f4bd7d87
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2024-10-23 17:51:50 +0530

    closure

--- day11Closure/index.js
+++ day11Closure/index.js
@@ -1,4 +1,5 @@
 //Closure
+//A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives a function access to its outer scope. In JavaScript, closures are created every time a function is created, at function creation time.
 
 function outer() {
   //lexical environment
