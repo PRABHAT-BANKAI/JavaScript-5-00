0758999ad95990c54cbfaeae0b5557dab406b1f9
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2024-11-20 17:44:40 +0530

    revision

--- revision/datatypes.js
+++ revision/datatypes.js
@@ -23,3 +23,71 @@ console.log(userName2);
 let bigInt = 1234346245480734987909509823n;
 
 console.log(bigInt);
+
+//non primitive
+let a = 123;
+
+let b = "hello";
+
+let c = false;
+let array = ["string", 12345, true, a, b, c]; //   element length = 6
+//  index       0        1      2    3  4  5
+
+/// array index always start with 0
+console.log(array[2]);
+console.log(array[4]);
+console.log(array.length);
+console.log(array);
+console.log(typeof array);
+
+// object carry key value pairs
+let object = {
+  userName: "batman",
+  age: "33",
+  city: "gautam city",
+};
+
+console.log(object);
+console.log(object.userName);
+
+//functions
+
+// console.log("1")
+// console.log("2")
+// console.log(bug)
+// console.log("3")
+// console.log("4")
+
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+
+
+//4
+//2
+//1
+//3
+
+
+function print1(){  // block 
+  console.log("1")
+}
+function print2(){
+  console.log("2")
+}
+function print3(){
+  console.log("3")
+}
+function print4(){
+  console.log("4")
+}
+
+print4()
+print2()
+print3()
+print1()
+
+
+
+
---
+++ revision/variables.js
@@ -0,0 +1,65 @@
+// what is variables understanding data types declaring and initialization variables
+
+// let const var
+
+let games = "pubg";
+
+console.log(games); // output pubg
+
+// Types Of Operator
+// Arithmetic operators
+// Assignment operators
+// Comparison operators
+// Logical operators
+// conversion with console.log and innerHTML
+
+// Arithmetic operators
+//addiition  +
+//substraction  -
+//multiplication  *
+//division    /
+//exponentiation  **
+// modulus   %
+
+let value1 = 45;
+let value2 = 5;
+
+console.log(value1 + value2);
+console.log(12 + 78);
+
+console.log(12 * 4);
+console.log(123 * 4);
+
+console.log(value1 / value2);
+
+console.log(2 ** 3);
+console.log(3 ** 3);
+
+console.log(67 % 3);
+
+// Assignment operators
+//=
+//+=
+//-=
+//*=
+// /=
+// %=
+// **=
+let subject = "javscript";
+
+let sum = 10;
+
+// sum = sum + 40;
+sum += 40
+console.log(sum);
+
+
+//exponentiation  **
+
+let number5 = 2
+// number5 = number5 ** 2
+number5 **= 2
+console.log(number5)
+
+
+console.log(5< 1) //false
