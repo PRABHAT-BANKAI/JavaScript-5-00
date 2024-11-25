3842514dc96dce186bbece55fdbf18e703484b4c
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2024-11-25 17:47:34 +0530

    ifelserevision

---
+++ revision/ifelse.js
@@ -0,0 +1,81 @@
+//syntax
+
+// if (condition) {
+//   // true or false
+// }
+
+// let number = 90;
+// {
+//   console.log(number);
+
+// }
+
+let percentage = 90;
+
+if (percentage > 90) {
+  console.log("you will get bike");
+} else {
+  console.log("pitai");
+}
+
+if (45 > 95) {
+  console.log("45 is greater than 95");
+} else {
+  console.log("45 is not greater than 95");
+}
+
+//
+
+let email = "asd@gmail.co";
+let password = "123as";
+
+if (email === "asd@gmail.com") {
+  if (password === "123asd") {
+    console.log("Login successful");
+  } else {
+    console.log("Incorrect password");
+  }
+} else {
+  console.log("Incorrect email");
+}
+
+//find smallest and largest
+
+let a = 70;
+let b = 100;
+let c = 15;
+
+if (a > b) {
+  // 70>100 false
+  if (a > c) {
+    console.log("a is largest number");
+  } else {
+    console.log("c is largest number");
+  }
+} else {
+  if (b > c) {
+    // 100>15
+    console.log("b is largest number");
+  } else {
+    console.log("c is largest number");
+  }
+}
+
+if (45 < 30) {
+  console.log("greater than");
+}
+if (34 < 1) {
+  console.log("less than");
+} else {
+  console.log("equal to");
+}
+
+let value = 25;
+
+//if()//{
+// console.log(thisis even)
+//}else{
+//odd
+//}
+
+
