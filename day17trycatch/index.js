0f2182c73847a832d1663edc4270e179458eb471
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2024-11-26 17:44:48 +0530

    trycatch

---
+++ day17trycatch/index.js
@@ -0,0 +1,68 @@
+// JS Error Throw and try to catch  // block
+
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log(hello); // temporal deadzone create  concept "hositing"
+// console.log("5");
+// console.log("6");
+
+// let hello = "hello "
+// try catch is used for error handling in javascript
+// try { syntax of try catch
+// } catch (error) {}
+
+console.log("1");
+console.log("2");
+console.log("3");
+try {
+  console.log(hello);
+} catch (error) {
+  console.log(error);
+}
+
+console.log("5");
+console.log("6");
+
+let number = 11;
+
+
+  // if (number % 2 == 0) {
+  //   console.log("even number");
+  // } else {
+  //   console.error("odd number");
+  // }
+
+
+
+  function print1(number) { // parameter
+    console.log(number)
+  }
+
+
+
+  print1(23) // argument
+
+  print1()
+
+  print1(78)
+
+  print1(45)
+
+
+try{
+  console.log("first line")
+}catch(error){
+  console.log(error)
+}
+
+  console.log("second line")
+  console.log("third line")
+ try{
+  console.log(fifth )
+ }catch(error){
+  console.log(error)
+ }
+  console.log("fourth line")
+
+
